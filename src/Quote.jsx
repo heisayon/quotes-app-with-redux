e6f747d19f29ed7bc562fac44f385804274f8233
Zion <heisayon@gmail.com>
2024-08-19 23:48:32 +0100

    fixed

--- src/Quote.jsx
+++ src/Quote.jsx
@@ -18,13 +18,13 @@ function Quote() {
               </section>
             );
           })}
+          <button
+            onClick={() => refetch()}
+            className="px-5 py-2 border-black border mt-5 active:scale-90 transition-all rounded-sm custom-shadow bg-blue-500 text-white"
+          >
+            Generate New
+          </button>
         </div>
-        <button
-          onClick={() => refetch()}
-          className="px-5 py-2 border-black border mt-5 active:scale-90 transition-all rounded-sm custom-shadow bg-blue-500 text-white"
-        >
-          Generate New
-        </button>
       </div>
     </>
   );
